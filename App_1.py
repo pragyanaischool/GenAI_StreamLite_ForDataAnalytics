2dd0dfbd6a29db5768e47931ca1f73a0a5024885
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2025-02-06 23:56:47 +0530

    Update App_1.py

--- App_1.py
+++ App_1.py
@@ -1,1 +1,78 @@
+#=======================================================================
+## 0. Importing libraries and setting up streamlit web app
 
+#Importing the necessary packages
+import streamlit as st
+import openpyxl
+import pygwalker as pyg
+import pandas as pd
+
+#Setting up web app page
+st.set_page_config(page_title='PragyanAI Exploratory Data Analysis App', page_icon=None, layout="wide")
+
+#Creating section in sidebar
+st.sidebar.write("****A) File upload****")
+
+#User prompt to select file type
+ft = st.sidebar.selectbox("*What is the file type?*",["Excel", "csv"])
+
+#Creating dynamic file upload option in sidebar
+uploaded_file = st.sidebar.file_uploader("*Upload file here*")
+
+if uploaded_file is not None:
+    file_path = uploaded_file
+
+    if ft == 'Excel':
+        try:
+            #User prompt to select sheet name in uploaded Excel
+            sh = st.sidebar.selectbox("*Which sheet name in the file should be read?*",pd.ExcelFile(file_path).sheet_names)
+            #User prompt to define row with column names if they aren't in the header row in the uploaded Excel
+            h = st.sidebar.number_input("*Which row contains the column names?*",0,100)
+        except:
+            st.info("File is not recognised as an Excel file")
+            sys.exit()
+    
+    elif ft == 'csv':
+        try:
+            #No need for sh and h for csv, set them to None
+            sh = None
+            h = None
+        except:
+            st.info("File is not recognised as a csv file.")
+            sys.exit()
+
+    #Caching function to load data
+    @st.cache_data()
+    def load_data(file_path,ft,sh,h):
+        
+        if ft == 'Excel':
+            try:
+                #Reading the excel file
+                data = pd.read_excel(file_path,header=h,sheet_name=sh,engine='openpyxl')
+            except:
+                st.info("File is not recognised as an Excel file.")
+                sys.exit()
+    
+        elif ft == 'csv':
+            try:
+                #Reading the csv file
+                data = pd.read_csv(file_path)
+            except:
+                st.info("File is not recognised as a csv file.")
+                sys.exit()
+        
+        return data
+
+    data = load_data(file_path,ft,sh,h)
+
+#=====================================================================================================
+## 1. Overview of the data
+    st.write( '### 1. Dataset Preview ')
+
+    try:
+      #View the dataframe in streamlit
+      st.dataframe(data, use_container_width=True)
+
+    except:
+      st.info("The file wasn't read properly. Please ensure that the input parameters are correctly defined.")
+      sys.exit()
